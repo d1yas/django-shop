e29ef350385441201ad87c7a9d8fe33e3d5f3fea
d1yas <diyasdjumabaevv@gmail.com>
2026-06-27 17:58:05 +0500

    edit settings.py

--- config/settings.py
+++ config/settings.py
@@ -127,8 +127,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'ProductsApp' / 'static']
+
+
+
+# Static files
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'ProductsApp' / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (uploaded images)
 MEDIA_URL = '/media/'
